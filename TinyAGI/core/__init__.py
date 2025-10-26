69b148f8f6048187b4cffc4079cad4f5b3b5fe5b
SullyGreene <sullygoesgreene@gmail.com>
2025-10-26 11:25:53 +0000

    feat(core): Initialize core package with BaseManager import

---
+++ TinyAGI/core/__init__.py
@@ -0,0 +1,12 @@
+# MIT License
+# Copyright (c) 2024 Sully Greene
+# Repository: https://github.com/SullyGreene
+# Profile: https://x.com/@SullyGreene
+
+# This file makes the 'core' directory a Python package.
+
+from .base_manager import BaseManager
+
+__all__ = [
+    'BaseManager'
+]
